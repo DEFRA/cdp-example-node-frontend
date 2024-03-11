8af506130c9673fb0c174c4d05b0b922899cae57
Ivar Abrahamsen <ivar.abrahamsen@defra.gov.uk>
2024-03-11 15:04:58 +0000

    build navigation with prefix

--- src/config/nunjucks/context/build-navigation.js
+++ src/config/nunjucks/context/build-navigation.js
@@ -6,10 +6,33 @@ function buildNavigation(request) {
   return [
     {
       text: 'Home',
-      url: appPathPrefix,
-      isActive: request.path === appPathPrefix
+      url: generatePath('/'),
+      isActive: request.path === generatePath('/')
+    },
+    {
+      text: 'About',
+      url: generatePath('/about'),
+      isActive: request.path === generatePath('/about')
+    },
+    {
+      text: 'Third',
+      url: generatePath('/third'),
+      isActive: request.path === generatePath('/third')
     }
   ]
 }
 
-export { buildNavigation }
+function generatePath(requestPath, prefix = appPathPrefix) {
+  const path = `${prefix}${requestPath}`
+  if (path === '' || path === '/') {
+    return '/'
+  } else if (path.startsWith('//')) {
+    return requestPath
+  } else if (requestPath === '/') {
+    return prefix
+  } else {
+    return path
+  }
+}
+
+export { buildNavigation, generatePath }
--- src/config/nunjucks/context/build-navigation.test.js
+++ src/config/nunjucks/context/build-navigation.test.js
@@ -1,19 +1,60 @@
-import { config } from '~/src/config'
-import { buildNavigation } from '~/src/config/nunjucks/context/build-navigation'
+import {
+  buildNavigation,
+  generatePath
+} from '~/src/config/nunjucks/context/build-navigation'
 
-const appPathPrefix = config.get('appPathPrefix')
 const mockRequest = ({ path = '' } = {}) => ({
   path
 })
 
 describe('#buildNavigation', () => {
-  test('Should provide expected highlighted navigation details', async () => {
-    expect(buildNavigation(mockRequest({ path: '' }))).toEqual([
+  test('Should provide expected highlighted navigation details', () => {
+    expect(buildNavigation(mockRequest({ path: '/' }))).toEqual([
       {
         isActive: true,
         text: 'Home',
-        url: appPathPrefix
+        url: '/'
+      },
+      {
+        isActive: false,
+        text: 'About',
+        url: '/about'
+      },
+      {
+        isActive: false,
+        text: 'Third',
+        url: '/third'
       }
     ])
+  })
+})
+
+describe('#prefixer', () => {
+  test('Should return / when prefix is empty and request path is empty', () => {
+    expect(generatePath('', '')).toEqual('/')
+  })
+  test('Should return / when prefix is empty and request path is /', () => {
+    expect(generatePath('/', '')).toEqual('/')
+  })
+  test('Should return /about when prefix is empty and request path is /about', () => {
+    expect(generatePath('/about', '')).toEqual('/about')
+  })
+  test('Should return / when prefix is / and request path is empty', () => {
+    expect(generatePath('', '/')).toEqual('/')
+  })
+  test('Should return / when prefix is / and request path is /', () => {
+    expect(generatePath('/', '/')).toEqual('/')
+  })
+  test('Should return /about when prefix is / and request path is /about', () => {
+    expect(generatePath('/about', '/')).toEqual('/about')
+  })
+  test('Should return /prefix when prefix is /prefix and request path is empty', () => {
+    expect(generatePath('', '/prefix')).toEqual('/prefix')
+  })
+  test('Should return /prefix when prefix is /prefix and request path is /', () => {
+    expect(generatePath('/', '/prefix')).toEqual('/prefix')
+  })
+  test('Should return /prefix/about when prefix is /prefix and request path is /about', () => {
+    expect(generatePath('/about', '/prefix')).toEqual('/prefix/about')
   })
 })
