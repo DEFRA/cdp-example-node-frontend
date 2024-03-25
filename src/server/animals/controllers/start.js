800dcb5e3b475a950467b15f46d2ab9742749dce
Ben Chidgey <ben@feedmypixel.com>
2024-03-25 18:57:55 +0000

    Add errors

--- src/server/animals/controllers/start.js
+++ src/server/animals/controllers/start.js
@@ -1,12 +1,22 @@
 import { sessionNames } from '~/src/server/common/constants/session-names'
 import { saveToAnimal } from '~/src/server/animals/helpers/form/save-to-animal'
+import { initUpload } from '~/src/server/common/helpers/upload/init-upload'
 
 const startController = {
   handler: async (request, h) => {
     request.yar.clear(sessionNames.animals)
     request.yar.clear(sessionNames.validationFailure)
 
-    await saveToAnimal(request, h, {})
+    const secureUpload = await initUpload({
+      successRedirect: 'http://localhost:3000/animals/add/your-details',
+      failureRedirect: 'http://localhost:3000/animals/add/upload-picture',
+      scanResultCallback: 'http://localhost:3000',
+      destinationBucket: 'my-bucket',
+      acceptedMimeTypes: ['.pdf', '.csv', '.png', 'image/jpeg'],
+      maxFileSize: 100
+    })
+
+    await saveToAnimal(request, h, { secureUpload })
 
     return h.redirect('/animals/add/details')
   }
--- src/server/common/helpers/upload/save-upload-status.js
+++ src/server/common/helpers/upload/save-upload-status.js
@@ -2,11 +2,16 @@ import { sessionNames } from '~/src/server/common/constants/session-names'
 import { fetchStatus } from '~/src/server/common/helpers/upload/fetch-status'
 import { saveToAnimal } from '~/src/server/animals/helpers/form/save-to-animal'
 
+// TODO needs a better name
 async function saveUploadStatus(request, h) {
   const animalsSession = request.yar.get(sessionNames.animals)
-  const uploadStatus = await fetchStatus(animalsSession.uploadId)
+  const uploadId = animalsSession?.secureUpload?.id
 
-  await saveToAnimal(request, h, { uploadStatus })
+  if (uploadId) {
+    const uploadStatus = await fetchStatus(uploadId)
+
+    await saveToAnimal(request, h, { uploadStatus })
+  }
 }
 
 export { saveUploadStatus }
