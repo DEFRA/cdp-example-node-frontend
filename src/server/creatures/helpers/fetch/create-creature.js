6810c59abbb7e12fbb0d01a6296d05c4eb13a944
Ben Chidgey <ben@feedmypixel.com>
2024-05-07 18:21:51 +0100

    A few more markup and summary tweaks

--- src/server/creatures/helpers/fetch/create-creature.js
+++ src/server/creatures/helpers/fetch/create-creature.js
@@ -1,42 +1,14 @@
 import { config } from '~/src/config'
 import { fetcher } from '~/src/server/common/helpers/fetch/fetcher'
+import { sessionToCreature } from '~/src/server/creatures/transformers/session-to-creature'
 
-function buildFilesDetail(value) {
-  const files = Array.isArray(value) ? value : [value]
-
-  return files.map((file) => ({
-    filename: file.filename,
-    fileId: file.fileId,
-    fileUrl: file.s3Key
-  }))
-}
-
-// TOO create transform and use in summary
-async function createCreature(creature) {
+async function createCreature(creatureSession) {
   const endpoint = config.get('cdpExampleNodeBackendUrl') + '/creatures'
-
-  const creatureFiles = buildFilesDetail(creature.fields.creatureFiles)
-  const evidenceFiles = buildFilesDetail(creature.fields.evidenceFiles)
-
-  const { year, month, day } = creature.fields.date
-  const date = new Date(year, parseInt(month, 10) - 1, day).toISOString()
+  const creature = sessionToCreature(creatureSession)
 
   const { json } = await fetcher(endpoint, {
     method: 'post',
-    body: JSON.stringify({
-      creatureId: creature.creatureId,
-      kind: creature.fields.kind,
-      creatureFiles,
-      date,
-      realLifeSighting: creature.fields.realLifeSighting === 'yes',
-      address: {
-        addressLine1: creature.fields?.addressLine1,
-        addressLine2: creature.fields?.addressLine2,
-        townOrCity: creature.fields?.addressTown,
-        postCode: creature.fields?.addressPostcode
-      },
-      evidenceFiles
-    })
+    body: JSON.stringify(creature)
   })
 
   return json
