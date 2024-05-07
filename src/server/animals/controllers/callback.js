ee689fc78108500297fc52ae1bfda624fb4e2dbb
Ben Chidgey <ben@feedmypixel.com>
2024-05-07 14:39:33 +0100

    Add Joi Date

--- src/server/animals/controllers/callback.js
+++
@@ -1,9 +0,0 @@
-const callbackController = {
-  handler: (request, h) => {
-    request.logger.info(request?.payload, 'Callback endpoint called for animal')
-
-    return request?.payload ?? {}
-  }
-}
-
-export { callbackController }
