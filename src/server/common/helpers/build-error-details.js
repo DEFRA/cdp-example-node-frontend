bc6159158eda233e9c4f43a31c65183bf9329237
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2024-05-14 11:45:30 +0100

    removes alterantives err workaround

--- src/server/common/helpers/build-error-details.js
+++ src/server/common/helpers/build-error-details.js
@@ -5,24 +5,10 @@ function buildErrorMessages(path, value) {
   return object
 }
 
-function extractMessage(detail) {
-  switch (detail.type) {
-    case 'alternatives.match':
-      // when alternatives fails it gives its own reason why each match stage failed
-      // the actual errors we want (file related) are in the context
-      return detail.context.details
-        .filter((d) => d.type !== 'object.base')
-        .map((d) => d.message)
-        .join(' ')
-    default:
-      return detail.message
-  }
-}
-
 function buildErrorDetails(errorDetails = []) {
   return errorDetails.reduce((errors, detail) => {
     const errorMessages = buildErrorMessages(detail.path, {
-      message: extractMessage(detail)
+      message: detail.message
     })
 
     return {
